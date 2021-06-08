75f8a723fbfedfb2b3b06c11621ef8da56670992
Romy <80348985+ROOMH@users.noreply.github.com>
2021-06-08 12:24:27 +0200

    Add files via upload

---
+++ 25_Opdracht_function_arguments/script.js
@@ -0,0 +1,8 @@
+const wallPaint = function(painted) {
+    console.log("The wall has been painted" + painted);
+    console.log("The wall on the");
+}
+
+/* wallPaint(painted);
+console.log("green"); */
+
